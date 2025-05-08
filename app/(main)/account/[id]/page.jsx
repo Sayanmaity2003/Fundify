2fdd3c94f6f2e2ec4f4e7109a5287a8891196149
Sayan Maity <sayanmaity2003@gmail.com>
2025-05-08 22:17:27 +0530

    Account Page Done

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { getAccountWithTransactions } from "@/actions/accounts";
+import { notFound } from "next/navigation";
+import React from "react";
 
-const AccountsPage = ({params}) => {
+const AccountsPage = async ({ params }) => {
+  const accountData = await getAccountWithTransactions(params.id);
+  if (!accountData) {
+    notFound();
+  }
+
+  const { transactions, ...account } = accountData;
+
   return (
-    <div>{params.id}</div>
-  )
-}
+    <div className="space-8 px-5 flex gap-4 items-end justify-between">
+      <div>
+        <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
+          {account.name}
+        </h1>
+        <p className="text-muted-foreground">
+          {account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account
+        </p>
+      </div>
 
-export default AccountsPage
+      <div className="text-right pb-2">
+        <div className="text-xl sm:text-2xl font-bold">
+          ₹{parseFloat(account.balance).toFixed(2)}
+        </div>
+        <p className="text-sm text-muted-foreground">
+          {account._count.transactions} Transactions
+        </p>
+      </div>
+
+      {/* Chart Section */}
+
+      {/* Transaction Table  */}
+    </div>
+  );
+};
+
+export default AccountsPage;
